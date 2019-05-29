d52681844fc2d5ec4ffe786f7e50f63e08a15242
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-05-29 16:59:04 -0300

    some validations to prevent user have more than one profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_one :address
-  
+  validates_uniqueness_of :user_id
   accepts_nested_attributes_for :address
 
   GENDER = [
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, presence: true
+  has_one :profile
 
 end
