cc69ce2fec5fb9b46db0e9a8d551d126d3fa2881
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-05-29 13:59:37 -0300

    devise signin path default changed, share partials: flashes added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	# after test's uncomment it
+	# before_action :authenticate_user!
+
+	# def after_sign_in_path_for(resource)
+	# 	stored_location_for(resource) || welcome_path
+	# end
 end
