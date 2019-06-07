1a7ffa9bbf7b1ff011b2377d22e8b5d45ed17b70
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-06-07 14:48:55 -0300

    infoWindow working

--- app/javascript/plugins/mapbox.js
+++ app/javascript/plugins/mapbox.js
@@ -17,24 +17,15 @@ const initMapbox = () => {
 
     const markers = JSON.parse(mapElement.dataset.markers);
 	  markers.forEach((marker) => {
+	  	const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 	    new mapboxgl.Marker()
 	      .setLngLat([ marker.lng, marker.lat ])
+	      .setPopup(popup)
 	      .addTo(map);
 	  });
 
 	  fitMapToMarkers(map, markers);
   }
-
-  const addMarkersToMap = (map, markers) => {
-	  markers.forEach((marker) => {
-	    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
-
-	    new mapboxgl.Marker()
-	      .setLngLat([ marker.lng, marker.lat ])
-	      .setPopup(popup) // add this
-	      .addTo(map);
-	  });
-	};
 };
 
 export { initMapbox };
